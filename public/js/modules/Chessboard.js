5b282214b1bf570adc81836d08082763f5fa2baf
sam <samuele2231@hotmail.com>
2022-06-28 12:24:12 +0200

    add drag and drop

--- public/js/modules/Chessboard.js
+++ public/js/modules/Chessboard.js
@@ -22,131 +22,179 @@ export class Chessboard{
         this.roi_w = new ChessPiece('roi','w');
         this.reine_b = new ChessPiece('reine','b');
         this.reine_w = new ChessPiece('reine','w');
-        this.img;
-        this.new_case;
-     
+
+    
+    }
+    click(){
+        console.log(new_case.pos_row,new_case.pos_col);
     }
 
-    update(){
+    // fonction qui cree le plateau de jeu
+    create(){
         
         for (let i = 0; i < this.plateau.length; i++) {
             for (let j = 0; j < this.plateau.length; j++) {
-                this.new_case = document.createElement("div");
-                
-                this.img = document.createElement("img");
-                this.img.id = `piece[${i}][${j}]`; 
-                this.img.draggable="true";
-                //this.img.ondragstart="this.dragStart(this)";
-                this.img.addEventListener('dragstart', this.dragStart);
-   
-                this.img.className+="piece_img"
+                let img = document.createElement("img");
+                img.draggable="true";
+                img.src = "";
+                img.id = "piece"+i+j
+                img.setAttribute("piece","");
+                img.addEventListener('dragstart', this.dragStart);
 
-                if (this.plateau[i][j] == "pionn"){
-                    
-                    this.img.src = this.pion_b.img;
-                    this.new_case.appendChild(this.img);
-                }else if(this.plateau[i][j] == "pionb"){
-                                    
-                
-                    this.img.src = this.pion_w.img;
-                    this.new_case.appendChild(this.img);
-            
-                }else if(this.plateau[i][j] == "Tn"){
-                  
-                    this.img.src = this.tour_b.img;
-                    this.new_case.appendChild(this.img);
-                }else if(this.plateau[i][j] == "foub"){
-                    
-                    this.img.src = this.fou_w.img;
-                    this.new_case.appendChild(this.img);
-                }
-                else if(this.plateau[i][j] == "Tb"){
-                    this.img.src = this.tour_w.img;
-                    this.new_case.appendChild(this.img);
-                }
-                else if(this.plateau[i][j] == "foun"){
-                    this.img.src = this.fou_b.img;
-                    this.new_case.appendChild(this.img);
-                }
-                else if(this.plateau[i][j] == "chevn"){
-            
-                    this.img.src = this.chev_b.img;
-                    this.new_case.appendChild(this.img);
-                }
-                else if(this.plateau[i][j] == "chevb"){
-                
-                    this.img.src = this.chev_w.img;
-                    this.new_case.appendChild(this.img);
-                }
-                else if(this.plateau[i][j] == "roib"){
-              
-                    this.img.src = this.roi_w.img;
-                    this.new_case.appendChild(this.img);
-                }
-                else if(this.plateau[i][j] == "roin"){
-                 
-                    this.img.src = this.roi_b.img;
-                    this.new_case.appendChild(this.img);
-                }
-                else if(this.plateau[i][j] == "reineb"){
-                
-                    this.img.src = this.reine_w.img;
-                    this.new_case.appendChild(this.img);
-                }
-                else if(this.plateau[i][j] == "reinen"){
-               
-                    this.img.src = this.reine_b.img;
-                    this.new_case.appendChild(this.img);
-                }
+                let new_case = document.createElement("div");
 
-                this.new_case.className+=this.color;
-                this.new_case.className+=" chess_case";
-                this.new_case.addEventListener('ondrop',  this.drop); 
-                this.new_case.addEventListener('ondragover',  this.allowDrop); 
-                // this.new_case.onfocus = this.focus();
-                this.new_case.style.width = this.w_board/8+"px";
-                this.new_case.style.height = this.w_board/8+"px";
-                this.board.appendChild(this.new_case); 
+                new_case.appendChild(img);
+                new_case.pos_row = i;
+                new_case.pos_col = j;
+                // new_case.onclick = "this.click()";
+
+                new_case.className+=this.color;
+                new_case.className+=" chess_case";
+
+                new_case.addEventListener('click', this.givePos); 
+
+                // drag and drop 
+                new_case.addEventListener('drop',  this.drop); 
+                new_case.addEventListener('dragover',  this.allowDrop); 
+        
+                new_case.style.width = this.w_board/8+"px";
+                new_case.style.height = this.w_board/8+"px";
+                this.board.appendChild(new_case); 
                
                 if (j != this.column-1){this.switchColor()}
-                this.plateau[i][j] = this.new_case;
+              
             }
             
         }
-        console.log(this.plateau);
+
     }
-    allowDrop(ev) {
-        ev.preventDefault();
+
+// methode qui met a jour le plateau
+    update(){
+        let domArray = this.toDArray(this.board.children);
+        for (let i = 0; i < this.plateau.length; i++) {
+            for (let j = 0; j < this.plateau.length; j++) {
+
+                if (this.plateau[i][j] == "pionn"){
+                    // on ajoute a chaque fois la proporete oiece qui vaut la piece adequate
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","tour");   
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.pion_b.img;
+
+                }else if(this.plateau[i][j] == "pionb"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","pion");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.pion_w.img;
+
+            
+                }else if(this.plateau[i][j] == "Tn"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","tour");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.tour_b.img;
+
+
+                }else if(this.plateau[i][j] == "foub"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","fou");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.fou_w.img;
+
+                }
+                else if(this.plateau[i][j] == "Tb"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","tour");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.tour_w.img;
+
+                }
+                else if(this.plateau[i][j] == "foun"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","fou");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.fou_b.img;
+
+                }
+                else if(this.plateau[i][j] == "chevn"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","chev");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.chev_b.img;
+
+                }
+                else if(this.plateau[i][j] == "chevb"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","chev");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.chev_w.img;
+
+                }
+                else if(this.plateau[i][j] == "roib"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","roi");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.roi_w.img;
+
+                }
+                else if(this.plateau[i][j] == "roin"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","roi");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.roi_b.img;
+
+                }
+                else if(this.plateau[i][j] == "reineb"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","reine");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.reine_w.img;
+
+                }
+                else if(this.plateau[i][j] == "reinen"){
+                    domArray[i][j].getElementsByTagName('img')[0].setAttribute("piece","reine");
+                    domArray[i][j].getElementsByTagName('img')[0].src = this.reine_b.img;
+
+                }
+  
+            }
         }
+             
+    }
+    allowDrop(e) {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = "move";
+    }
     dragStart(e) {
-        e.dataTransfer.setData('text/plain', e.target.id);
+        console.log( e.target)
+        e.dataTransfer.setData('text', e.target.id);
+        e.dataTransfer.dropEffect = "move";
         console.log("drag over");
     }
     drop(e) {
-    
+
+
+        e.preventDefault();
+        console.log("drag down");
         // get the draggable element
-        const id = ev.dataTransfer.getData("text");
+        const id = e.dataTransfer.getData("text/plain");
+        e.dataTransfer.dropEffect = "move";
         const draggable = document.getElementById(id);
-    
+        // console.log(e.target)
+        // console.log(draggable);
         // add it to the drop target
+        console.log(draggable);
         e.target.appendChild(draggable);
     
-        // display the draggable element
-        draggable.classList.remove('hide');
-        console.log("drag down");
+
+        // let parent = e.target.parentNode;
+        // parent.getElementsByTagName("img")[0].src="";
+        // console.log("parent",parent);
+
+        
     }
 
-    focus(){
-        this.new_case.style.border="2px solid orange";
+    givePos(){
+       console.log(this.pos_col,this.pos_row);
+       console.log(this);
     }
 
     switchColor(){
         this.color ? this.color = false : this.color = true;
     }
-
-
-
-
+    
+    toDArray(liste){
+        let ret = [];
+        let array = [];
+        for (let i = 0; i < liste.length+1; i++) {
+            if(i%8 == 0 & i!= 0){
+                ret.push(array);
+                array = []; 
+            }
+            array.push(liste[i]);
+        }
+  
+        return ret;
+    }
 
 }
 
